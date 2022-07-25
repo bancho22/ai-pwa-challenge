468178ec9f5ff30fda3e76f7d1f143eb2c74a3e0
Bancho Petrov <bancho_petrov@epam.com>
2022-07-25 17:03:17 +0300

    Home Screen Updated

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Machine Learning Playing Card Detection Challenge
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tensorflow.org/js"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn TensorFlowJS
         </a>
       </header>
     </div>
