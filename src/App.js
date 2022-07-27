140fb8ee7d828267256614a20ff4c9a48d0ae0b1
Bancho Petrov <bancho_petrov@epam.com>
2022-07-27 11:52:33 +0300

    Coco-ssd object detection via webcam incorporated

--- src/App.js
+++ src/App.js
@@ -1,82 +1,129 @@
 import React, { useState, useEffect, useRef } from "react";
+import Webcam from "react-webcam";
+// eslint-disable-next-line no-unused-vars
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import { drawRect } from "./utilities";
 import logo from "./logo.svg";
 import "./App.css";
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import * as tf from "@tensorflow/tfjs";
-import Webcam from "react-webcam";
 
 function App() {
-  const [img, setImg] = useState(null);
-  const [model, setModel] = useState();
-  const webcamRef = React.useRef(null);
-  const [videoWidth, setVideoWidth] = useState(960);
-  const [videoHeight, setVideoHeight] = useState(640);
+  const [isCamOn, setCamOn] = useState(false);
+  const intervalId = useRef(null);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
 
-  async function loadModel() {
-    try {
-      const model = await cocoSsd.load();
-      setModel(model);
-      console.log("set loaded Model");
-    } catch (err) {
-      console.log(err);
-      console.log("failed load model");
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      const obj = await net.detect(video);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      // 5. TODO - Update drawing utility
+      drawRect(obj, ctx);
+      console.log(obj); // printing predictions
     }
-  }
-  useEffect(() => {
-    tf.ready().then(() => {
-      loadModel();
-    });
-  }, []);
-
-  const videoConstraints = {
-    height: 500,
-    width: 500,
-    facingMode: "environment",
   };
+
+  useEffect(() => {
+    // Main function
+    const runCoco = async () => {
+      // 3. TODO - Load network 
+      console.log('Loading neural network...');
+      const net = await cocossd.load();
+      console.log('Neural network loaded!');
+      //  Loop and detect hands
+      intervalId.current = setInterval(() => {
+        detect(net);
+      }, 10);
+
+      return () => {
+        clearInterval(intervalId.current);
+      };
+    };
+    runCoco();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      <div style={{ position: "absolute", top: "400px" }}>
-          <Webcam
-            audio={false}
-            id="img"
-            ref={webcamRef}
-            screenshotQuality={1}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-          />
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Machine Learning Playing Card Detection Challenge</p>
-        <a
-          className="App-link"
-          href="https://www.tensorflow.org/js"
-          target="_blank"
-          rel="noopener noreferrer"
+        {
+          !isCamOn && (
+            <React.Fragment>
+              <img src={logo} className="App-logo" alt="logo" />
+                <p>Machine Learning Playing Card Detection Challenge</p>
+                <a
+                  className="App-link"
+                  href="https://www.tensorflow.org/js"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn TensorFlowJS
+                </a>
+            </React.Fragment>
+          )
+        }
+        <button
+          className={`detection-button${isCamOn ? '-top' : ''}`}
+          onClick={() => setCamOn(prevState => !prevState)}
         >
-          Learn TensorFlowJS
-        </a>
-        {!Boolean(img) && (
-          <div className="input-wrapper">
-            <span>Insert playing card image below:</span>
-            <input
-              type="file"
-              onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))}
-            />
-          </div>
-        )}
-        {Boolean(img) && (
-          <React.Fragment>
-            <img src={img} alt="uploaded card" className="card-image" />
-            <button
-              className="detection-button"
-              onClick={() => alert("Implement me!")}
-            >
-              Run Detection Algo
-            </button>
-          </React.Fragment>
-        )}
+          {`${isCamOn ? 'Stop' : 'Start'} cam`}
+        </button>
+        {
+          isCamOn && (
+            <React.Fragment>
+              <Webcam
+                ref={webcamRef}
+                muted={true}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 9,
+                  width: 640,
+                  height: 480,
+                }}
+              />
+              <canvas
+                ref={canvasRef}
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  left: 0,
+                  right: 0,
+                  textAlign: "center",
+                  zindex: 8,
+                  width: 640,
+                  height: 480,
+                }}
+              />
+            </React.Fragment>
+          )
+        }
       </header>
     </div>
   );
